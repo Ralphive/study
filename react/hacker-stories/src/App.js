8950339529be108d5203d7bca6a8e7a3c3e5aff3
Ralphive <ralphive@protonmail.com>
2022-01-23 19:22:05 +0000

    React state sample

--- react/hacker-stories/src/App.js
+++ react/hacker-stories/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import React from 'react';
+
 
 const welcome = {
     greeting: "Hey",
@@ -9,52 +11,54 @@ const getTitle = (title) => {
     return title;
 };
 
+
 const App = () => {
-    const stories = [
-        {
-            title: "React",
-            url: "https://reactjs.org/",
-            author: "Jordan Walke",
-            num_comments: 3,
-            points: 4,
-            objectID: 0,
-        },
-        {
-            title: "Redux",
-            url: "https://redux.js.org/",
-            author: "Dan Abramov, Andrew Clark",
-            num_comments: 2,
-            points: 5,
-            objectID: 1,
-        },
-    ];
+  const [searchTerm, setSearchTerm] = React.useState('');
 
-    return (
-        <div>
-            <Header />
+  const stories = [
+    {
+        title: "React",
+        url: "https://reactjs.org/",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: "Redux",
+        url: "https://redux.js.org/",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+  ];
 
-            <h2>{getTitle("Function Result")} as a title 2</h2>
-            <p>Here is an array, but as a component</p>
-            <List list={stories} test="This is a Test" />
-        </div>
-    );
-};
-const handleChange = (event) => {
+  const handleChange = (event) => {
     // console.log(event)
-    console.log(event.target.value);
-};
+    // console.log(event.target.value);
+    setSearchTerm(event.target.value);
 
-const Header = () => {
-    return (
-        <header>
+  };
+
+  return (
+      <div>
+          <header>
             <h1>
                 {welcome.greeting} {welcome.title}
             </h1>
             <label htmlFor="search">Busca: </label>
             <input id="search" type="text" onChange={handleChange} />
-        </header>
-    );
+            <span> Searching for <strong>{searchTerm}</strong>.</span>
+          </header>
+
+          <h2>{getTitle("Function Result")} as a title 2</h2>
+          <p>Here is an array, but as a component</p>
+          <List list={stories} test="This is a Test" />
+      </div>
+  );
 };
+
 
 const List = (props) => {
     return props.list.map((item) => {
