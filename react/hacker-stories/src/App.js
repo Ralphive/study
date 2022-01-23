c9750f88a9019d7fccbaf7abae0d575d44d66c6d
Ralphive <ralphive@protonmail.com>
2022-01-23 19:10:58 +0000

    formatting

--- react/hacker-stories/src/App.js
+++ react/hacker-stories/src/App.js
@@ -5,31 +5,29 @@ const welcome = {
     title: "React",
 };
 
-
-
 const getTitle = (title) => {
     return title;
-}
+};
 
 const App = () => {
-  const stories = [
-    {
-        title: "React",
-        url: "https://reactjs.org/",
-        author: "Jordan Walke",
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: "Redux",
-        url: "https://redux.js.org/",
-        author: "Dan Abramov, Andrew Clark",
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
+    const stories = [
+        {
+            title: "React",
+            url: "https://reactjs.org/",
+            author: "Jordan Walke",
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: "Redux",
+            url: "https://redux.js.org/",
+            author: "Dan Abramov, Andrew Clark",
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
 
     return (
         <div>
@@ -40,12 +38,11 @@ const App = () => {
             <List list={stories} test="This is a Test" />
         </div>
     );
-}
-const handleChange = event =>{
-  // console.log(event)
-  console.log(event.target.value);
-
-}
+};
+const handleChange = (event) => {
+    // console.log(event)
+    console.log(event.target.value);
+};
 
 const Header = () => {
     return (
@@ -57,13 +54,13 @@ const Header = () => {
             <input id="search" type="text" onChange={handleChange} />
         </header>
     );
-}
+};
 
-const List = props => {
+const List = (props) => {
     return props.list.map((item) => {
         return (
             <div key={item.objectID}>
-                x - {props.test} - 
+                x - {props.test} -
                 <span>
                     <a href={item.url}>{item.title}</a>
                 </span>
@@ -73,7 +70,6 @@ const List = props => {
             </div>
         );
     });
-}
+};
 
 export default App;
-
