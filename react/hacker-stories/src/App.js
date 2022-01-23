b91e8b4c04dfc1744639e50e6f3777d963a5445a
Ralphive <ralphive@protonmail.com>
2022-01-23 18:03:26 +0000

    array display

--- react/hacker-stories/src/App.js
+++ react/hacker-stories/src/App.js
@@ -1,24 +1,49 @@
-import './App.css';
+import "./App.css";
 
 const welcome = {
-  greeting: 'Hey',
-  title: 'React',
+    greeting: "Hey",
+    title: "React",
 };
 
+const list = [
+    {
+        title: "React",
+        url: "https://reactjs.org/",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: "Redux",
+        url: "https://redux.js.org/",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
 function getTitle(title) {
-  return title;
+    return title;
 }
 
 function App() {
-  return (
-  <div>
-    <h1>
-      {welcome.greeting} {welcome.title}
-    </h1>
-    <h2>{getTitle('Function Result')} as a title 2</h2>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-  );
+    return (
+        <div>
+            <h1>
+                {welcome.greeting} {welcome.title}
+            </h1>
+            <h2>{getTitle("Function Result")} as a title 2</h2>
+            <p>
+                Here is an array{" "}
+                {list.map((item) => {
+                    return <div>{item.title}</div>;
+                })}
+            </p>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+    );
 }
 export default App;
