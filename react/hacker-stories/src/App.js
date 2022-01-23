8c467fb83c6ddc764b75a6c442ec633b27277715
Ralphive <ralphive@protonmail.com>
2022-01-23 18:18:35 +0000

    list many properties of an array item

--- react/hacker-stories/src/App.js
+++ react/hacker-stories/src/App.js
@@ -38,7 +38,16 @@ function App() {
             <p>
                 Here is an array{" "}
                 {list.map((item) => {
-                    return <div>{item.title}</div>;
+                    return (
+                        <div key={item.objectID}>
+                            <span>
+                                <a href={item.url}>{item.title}</a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </div>
+                    );
                 })}
             </p>
             <label htmlFor="search">Search: </label>
