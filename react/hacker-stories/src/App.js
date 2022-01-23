a03671ce349090dc5d17059994a2b03ed8b073e5
Ralphive <ralphive@protonmail.com>
2022-01-23 17:48:08 +0000

    using functions and vars in JSX

--- react/hacker-stories/src/App.js
+++ react/hacker-stories/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Really?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>
+      {welcome.greeting} {welcome.title}
+    </h1>
+    <h2>{getTitle('Function Result')} as a title 2</h2>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
   );
 }
-
 export default App;
